ef696f3c707a66c9c61c728fc0d70b29b52774e7
S1BERKAR <berru.karakas@accenture.com>
2023-08-02 20:28:02 +0300

    Add RabbitMq comm to pyspark module

---
+++ server/controllers/clothes.js
@@ -0,0 +1,44 @@
+import amqp from 'amqplib';
+
+// Simulate sending a message to the Python server via RabbitMQ
+async function sendRPCMessage(message) {
+  const connection = await amqp.connect('amqp://localhost');
+  const channel = await connection.createChannel();
+
+  await channel.assertQueue('rpc_queue', { durable: true });
+  const correlationId = generateUuid();
+  const resultPromise = new Promise((resolve) => {
+    channel.consume('', (msg) => {
+      if (msg.properties.correlationId === correlationId) {
+        resolve(msg.content.toString());
+      }
+    }, { noAck: true });
+  });
+
+  await channel.assertQueue('rpc_queue');
+  
+  channel.sendToQueue('rpc_queue', Buffer.from(message), {
+    correlationId,
+    replyTo: 'rpc_queue',
+  });
+
+  return resultPromise;
+}
+
+function generateUuid() {
+  return Math.random().toString() + Math.random().toString() + Math.random().toString();
+}
+
+export const getFilteredClothes = async (req, res) => {
+    const { query } = req.query;
+
+    sendRPCMessage(query)
+    .then((response) => {
+        console.log(`Received response from Python server: ${response}`);
+        res.status(200).json(response);
+    })
+    .catch((error) => {
+        res.status(404).json( {message : `Error communicating with Python server: ${error.message}`});
+    });
+
+}
--- server/index.js
+++ server/index.js
@@ -25,5 +25,3 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.CONNECTION_URL)
 .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
 .catch((error) => console.log(error.message));
-
-
